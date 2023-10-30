624cdea5c1877c37340f89509d87cec4bb7fb7e2
esapratama <ptrprtm16@gmail.com>
2023-10-30 13:40:14 +0700

    menampilkan panjang setiap baris

--- JOBSHEET 10/Bioskop10.java
+++ JOBSHEET 10/Bioskop10.java
@@ -12,11 +12,12 @@ public static void main(String[] args) {
     penonton[2][0] = "Eka";
     penonton[2][1] = "Farhan";
     penonton[3][0] = "Gisel";
-    penonton[3][1] = "Jaemin";
+    penonton[3][1] = "Hana";
 
-    System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-    System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-    System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-    System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+    System.out.println(penonton.length);
+
+    for (String[] barisPenonton : penonton) {
+        System.out.println("Panjang baris ke-" + barisPenonton.length);
+    }
  }
 }
