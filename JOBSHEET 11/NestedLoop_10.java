f8bf03ee076d435b7222f5434aba8a1a8ad878dd
esapratama <ptrprtm16@gmail.com>
2023-11-06 16:07:24 +0700

    modifikasi

--- JOBSHEET 11/NestedLoop_10.java
+++ JOBSHEET 11/NestedLoop_10.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 public class NestedLoop_10 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        double[][] arrayEsa = new double[2][3];
+        Scanner scan10 = new Scanner(System.in);
+        double[][] arrayEsa = new double[5][7];
         for (int i = 0; i < arrayEsa.length; i++) {
             System.out.println("Kota ke-" + i);
             for (int j = 0; j < arrayEsa[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
-                arrayEsa[i][j] = sc.nextDouble();
+                arrayEsa[i][j] = scan10.nextDouble();
             }
             System.out.println();
         }
         for (int i = 0; i < arrayEsa.length; i++) {
-            System.out.println("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < arrayEsa[0].length; j++) {
-                System.out.print(arrayEsa[i][j] + " ");
+            System.out.println("Kota ke-" + i);
+            double totalTemp = 0;
+            for (double temp : arrayEsa[i]) {
+                totalTemp += temp;
+                System.out.println(temp);
             }
-            System.out.println();
+            double rerataarrayEsa = totalTemp / arrayEsa[i].length;
+            System.out.println("\nRata-rata: " + rerataarrayEsa +"\n");
         }
+        scan10.close();
     }
 
 }
