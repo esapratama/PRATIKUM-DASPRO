fb0cb96b075edc9285b0f0f852e788ec20dfb4c9
esapratama <ptrprtm16@gmail.com>
2023-11-08 10:54:43 +0700

    tugas individu

--- JOBSHEET 11/TugasIndividu410.java
+++ JOBSHEET 11/TugasIndividu410.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class TugasIndividu410 {
     public static void main(String[] args) {
         Scanner sc10 = new Scanner(System.in);
-        System.out.print("Masukkan tinggi segitiga: ");
-        int tinggi = sc10.nextInt();
+        System.out.print("Menghitung hasil perkalian: ");
 
-        for (int i = tinggi; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) { 
+                int hasil = i * j;
+                System.out.print(i + " x " + j + " = " + hasil + "\t");
             }
             System.out.println();
         }
