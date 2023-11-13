04678eca0ecb444c34f950b53daaba1e255ab49b
esapratama <ptrprtm16@gmail.com>
2023-11-13 13:03:59 +0700

    Shorting

--- JOBSHEET 11/TugasIndividu410.java
+++ JOBSHEET 11/TugasIndividu410.java
@@ -6,9 +6,9 @@ public class TugasIndividu410 {
         System.out.print("Menghitung hasil perkalian: ");
 
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) { 
+            for (int j = 1; j <= 10; j++) {  
                 int hasil = i * j;
-                System.out.print(i + " x " + j + " = " + hasil + "\t");
+                System.out.println(i + " x " + j + " = " + hasil + " \t ");
             }
             System.out.println();
         }
