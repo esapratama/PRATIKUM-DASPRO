d2a8cac026992f09be3ebdc59f67fe4dac93e63c
esapratama <ptrprtm16@gmail.com>
2023-11-20 11:34:23 +0700

    Ucapan terimkasih

---
+++ JOBSHEET 13/TerimaKasih_10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TerimaKasih_10 {
+    public static void main(String[] args) {
+            UcapanTerimakasih();
+        }
+
+    public static void UcapanTerimakasih () {
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+
+    }
+    
+}
