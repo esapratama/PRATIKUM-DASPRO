484ce5eda1b7cd5eb1f75674d1a2caf10da8c5b7
esapratama <ptrprtm16@gmail.com>
2023-11-20 12:58:49 +0700

    string int

--- JOBSHEET 13/Ucapan_10.java
+++ JOBSHEET 13/Ucapan_10.java
@@ -10,7 +10,6 @@ public class Ucapan_10 {
     public static void main(String[] args) {
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+nama+"\nMay the force be with you."); {
-
         }
     }
 }
